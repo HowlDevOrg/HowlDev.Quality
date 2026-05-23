09a7d8fc69f963c1002c2a101df26ed645a65a63
Cody Howell <codyhowell44@gmail.com>
2026-05-23 14:24:26 -0600

    XML comments

--- HowlDev.Quality.Benchmarking/BenchmarkProfiles.cs
+++ HowlDev.Quality.Benchmarking/BenchmarkProfiles.cs
@@ -4,19 +4,35 @@ using BenchmarkDotNet.Jobs;
 namespace HowlDev.Quality.Benchmarking;
 
 /// <summary>
-/// A few default options to pass in as parameters. 
+/// Some default profiles to use in the <see cref="BenchmarkValidator&lt;T&gt;.WithProfile(ManualConfig)"/>
+/// function. Provides each level of jobs with a basic and a Silent version. 
 /// </summary>
 public static class BenchmarkProfiles {
+    /// <summary>
+    /// Adds the [ShortRunJob] attribute to the class. 
+    /// </summary>
     public static ManualConfig ShortRun => ManualConfig.CreateMinimumViable().AddJob(Job.ShortRun);
-    public static ManualConfig SilentShortRun => ManualConfig.CreateEmpty().AddJob(Job.ShortRun);
+    /// <summary>
+    /// Adds the [ShortRunJob] attribute to the class. Displays nothing to the console and 
+    /// writes no log outputs (but does not interfere with exporters).
+    /// </summary>
+    public static ManualConfig SilentShortRun => ManualConfig.CreateEmpty().AddJob(Job.ShortRun).WithOptions(ConfigOptions.DisableLogFile);
+    /// <summary>
+    /// Adds the [MediumRunJob] attribute to the class. 
+    /// </summary>
     public static ManualConfig MediumRun => ManualConfig.CreateMinimumViable().AddJob(Job.MediumRun);
-    public static ManualConfig SilentMediumRun => ManualConfig.CreateEmpty().AddJob(Job.MediumRun);
-    public static ManualConfig Longun => ManualConfig.CreateMinimumViable().AddJob(Job.LongRun);
-    public static ManualConfig SilentLongRun => ManualConfig.CreateEmpty().AddJob(Job.LongRun);
-    public static ManualConfig CI => SilentShortRun;
+    /// <summary>
+    /// Adds the [MediumRunJob] attribute to the class. Displays nothing to the console and 
+    /// writes no log outputs (but does not interfere with exporters).
+    /// </summary>
+    public static ManualConfig SilentMediumRun => ManualConfig.CreateEmpty().AddJob(Job.MediumRun).WithOptions(ConfigOptions.DisableLogFile);
+    /// <summary>
+    /// Adds the [LongRunJob] attribute to the class. 
+    /// </summary>
+    public static ManualConfig LongRun => ManualConfig.CreateMinimumViable().AddJob(Job.LongRun);
+    /// <summary>
+    /// Adds the [LongRunJob] attribute to the class. Displays nothing to the console and 
+    /// writes no log outputs (but does not interfere with exporters).
+    /// </summary>
+    public static ManualConfig SilentLongRun => ManualConfig.CreateEmpty().AddJob(Job.LongRun).WithOptions(ConfigOptions.DisableLogFile);
 }
-
-/*
-var config = ManualConfig.CreateMinimumViable()
-                .WithOptions(ConfigOptions.DisableLogFile);
-*/
